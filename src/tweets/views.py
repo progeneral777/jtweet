69600a4795be4a32181e2b69f4c62d85ccae563c
Jimmy <jimmytshs42@gmail.com>
2018-12-30 01:28:27 +0800

    add templates

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .models import Tweet
 # Create your views here.
 def tweet_detail_view(request,id = 1):
-	return render(request,"tweets/detail_view.html",{})
+	obj = Tweet.objects.get(id = id )
+	context = { "object" : obj,'abc':obj}
+	print(obj)
 
-def tweet_list_view(request,id = 1):
-	return render(request,"tweets/list_view.html",{})
+	return render(request,"tweets/detail_view.html",context)
+
+def tweet_list_view(request):
+	queryset = Tweet.objects.all()
+	print(queryset)
+	for obj in queryset:
+		print(obj.content)
+	context = { "object_list" : queryset}
+	return render(request,"tweets/list_view.html",context)
